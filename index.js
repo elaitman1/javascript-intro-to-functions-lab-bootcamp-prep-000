b4ed4bcb46a2f872cef73b8104b1b7fbdd8293eb
elaitman1 <elaitman1@gmail.com>
2018-07-16 16:41:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,11 +24,7 @@ function sayHiToGrandma(string)
 {
   if (string === string.toLowerCase()){
     return "I can't hear you!";
-  }    
-}
-function sayHiToGrandma(string) 
-{
-  if(string === "I love you, Grandma.") {
+  } else if(string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
