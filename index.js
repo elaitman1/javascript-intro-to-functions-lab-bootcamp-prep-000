012817da23f8bbdb9f83fb8d9b30897c7357cf4b
elaitman1 <elaitman1@gmail.com>
2018-07-16 16:42:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,23 +14,14 @@ function logWhisper(string)
 {
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string)
-{
-  if (string.toLowerCase()){
-    return "I can't hear you!"
-  }
-}
+
 function sayHiToGrandma(string)
 {
   if (string === string.toLowerCase()){
     return "I can't hear you!";
   } else if(string === "I love you, Grandma.") {
     return "I love you, too.";
-  }
-}
-function sayHiToGrandma(string)
-{
-  if (string.toUpperCase()){
+  } else if (string.toUpperCase()){
     return "YES INDEED!";
   }
 }
